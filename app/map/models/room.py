83be1653dea71319a4f103d64f835feece56fca8
Aleksey/Bishopik <deadkniga@gmail.com>
2025-03-20 21:44:19 +0300

    fix minor track

--- app/map/models/room.py
+++ app/map/models/room.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Text, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, Float, JSON
 from sqlalchemy.orm import relationship
 from app.database.database import Base
 
