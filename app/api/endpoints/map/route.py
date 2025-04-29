14e78b38fe7de1dbca4ee71015f956db6730b8b2
Aleksey/Bishopik <deadkniga@gmail.com>
2025-04-29 21:55:53 +0300

    V.0021 add room searching

--- app/api/endpoints/map/route.py
+++ app/api/endpoints/map/route.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database.database import get_db
-from app.map.pathfinder.pathfinder import find_path
+from app.map.utils.pathfinder import find_path
 from app.map.models.room import Room
 from app.map.models.segment import Segment
 from app.map.models.outdoor_segment import OutdoorSegment
